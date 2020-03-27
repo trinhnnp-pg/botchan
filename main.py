4376938e85d73a410089f537d50ba8608c495102
TrinhNNP <nnpt0808@gmail.com>
2020-03-27 16:06:49 +0900

    ADD request handling code

--- main.py
+++ main.py
@@ -1,7 +1,61 @@
-from flask import Flask 
-  
+from flask import Flask, request, Response
+import os
+import requests
+import json
+import random
+
 app = Flask(__name__) 
   
 @app.route("/")
 def route():
     return "<h1>Welcome to Chatbot</h1>"
+
+# env_variables
+# token to verify that this bot is legit
+verify_token = os.environ.get('VERIFY_TOKEN', None)
+# token to send messages through facebook messenger
+access_token = os.environ.get('ACCESS_TOKEN', None)
+
+@app.route('/webhook', methods=['GET'])
+def webhook_verify():
+    if request.args.get('hub.verify_token') == verify_token:
+        return request.args.get('hub.challenge')
+    return "Wrong verify token"
+
+@app.route('/webhook', methods=['POST'])
+def webhook_action():
+    data = json.loads(request.data.decode('utf-8'))
+    for entry in data['entry']:
+        user_message = entry['messaging'][0]['message']['text']
+        user_id = entry['messaging'][0]['sender']['id']
+        response = {
+            'recipient': {'id': user_id},
+            'message': {}
+        }
+        response['message']['text'] = handle_message(user_id, user_message)
+    return Response(response="EVENT RECEIVED",status=200)
+
+@app.route('/webhook_dev', methods=['POST'])
+def webhook_dev():
+    # custom route for local development
+    data = json.loads(request.data.decode('utf-8'))
+    user_message = data['entry'][0]['messaging'][0]['message']['text']
+    user_id = data['entry'][0]['messaging'][0]['sender']['id']
+    response = {
+        'recipient': {'id': user_id},
+        'message': {'text': handle_message(user_id, user_message)}
+    }
+    return Response(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+    )
+
+def handle_message(user_id, user_message):
+    # DO SOMETHING with the user_message ... ¯\_(ツ)_/¯
+    return "Hello "+user_id+" ! You just sent me : " + user_message
+
+@app.route('/privacy', methods=['GET'])
+def privacy():
+    # needed route if you need to make your bot public
+    return "This facebook messenger bot's only purpose is to [...]. That's all. We don't use it in any other way."
